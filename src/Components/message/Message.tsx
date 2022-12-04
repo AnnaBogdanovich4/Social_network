78c48b22c1f6b4862d04e5bc1251e99b392156d7
Anna Bogdanovich <bogdanovicanna4@gmail.com>
2022-12-05 02:57:43 +0300

    Add data into files

--- src/Components/message/Message.tsx
+++ src/Components/message/Message.tsx
@@ -3,23 +3,41 @@ import {Dialogs} from './dialogs/Dialogs';
 import {Correspondent} from './correspondent/Correspondent';
 
 export const Message = () => {
+    const correspondentData = [
+        {id: '1', name: 'Anna', src: 'https://klike.net/uploads/posts/2019-03/1551511823_2.jpg'},
+        {id: '2', name: 'Dima', src: 'https://klike.net/uploads/posts/2019-03/1551515594_15.jpg'},
+        {id: '3', name: 'Milana', src: 'https://klike.net/uploads/posts/2019-03/1551511808_5.jpg'},
+        {id: '4', name: 'Diana', src: 'https://klike.net/uploads/posts/2019-03/medium/1551511866_11.jpg'},
+        {id: '5', name: 'Alex', src: 'https://klike.net/uploads/posts/2019-03/1551511862_19.jpg'},
+        {id: '6', name: 'Esmi', src: 'https://klike.net/uploads/posts/2019-03/medium/1551511789_7.jpg'},
+    ]
+    const dialogsData = [
+        {id: '1', src: 'https://klike.net/uploads/posts/2019-03/medium/1551511784_4.jpg', message: 'Hi!'},
+        {id: '2', src: 'https://klike.net/uploads/posts/2019-03/1551511808_5.jpg', message: 'Hi'},
+        {id: '3', src: 'https://klike.net/uploads/posts/2019-03/medium/1551511784_4.jpg', message: 'I love you'},
+        {id: '4', src: 'https://klike.net/uploads/posts/2019-03/1551511808_5.jpg', message: 'Me too'},
+    ]
     return (
         <div className={style.messages}>
             <div className={style.correspondents}>
-                <Correspondent id="1" name="Anna" src="https://klike.net/uploads/posts/2019-03/1551511823_2.jpg"/>
-                <Correspondent id="2" name="Dima" src="https://klike.net/uploads/posts/2019-03/1551515594_15.jpg"/>
-                <Correspondent id="3" name="Milana" src="https://klike.net/uploads/posts/2019-03/1551511808_5.jpg"/>
-                <Correspondent id="4" name="Diana"
-                               src="https://klike.net/uploads/posts/2019-03/medium/1551511866_11.jpg"/>
-                <Correspondent id="5" name="Alex" src="https://klike.net/uploads/posts/2019-03/1551511862_19.jpg"/>
-                <Correspondent id="6" name="Esmi"
-                               src="https://klike.net/uploads/posts/2019-03/medium/1551511789_7.jpg"/>
+                <Correspondent id={correspondentData[0].id} name={correspondentData[0].name}
+                               src={correspondentData[0].src}/>
+                <Correspondent id={correspondentData[1].id} name={correspondentData[1].name}
+                               src={correspondentData[1].src}/>
+                <Correspondent id={correspondentData[2].id} name={correspondentData[2].name}
+                               src={correspondentData[2].src}/>
+                <Correspondent id={correspondentData[3].id} name={correspondentData[3].name}
+                               src={correspondentData[3].src}/>
+                <Correspondent id={correspondentData[4].id} name={correspondentData[4].name}
+                               src={correspondentData[4].src}/>
+                <Correspondent id={correspondentData[5].id} name={correspondentData[5].name}
+                               src={correspondentData[5].src}/>
             </div>
             <div className={style.dialogs}>
-                <Dialogs src="https://klike.net/uploads/posts/2019-03/medium/1551511784_4.jpg" message='Hi!'/>
-                <Dialogs src="https://klike.net/uploads/posts/2019-03/1551511808_5.jpg" message='Hi'/>
-                <Dialogs src="https://klike.net/uploads/posts/2019-03/medium/1551511784_4.jpg" message='I love you'/>
-                <Dialogs src="https://klike.net/uploads/posts/2019-03/1551511808_5.jpg" message='Me too'/>
+                <Dialogs src={dialogsData[0].src} message={dialogsData[0].message}/>
+                <Dialogs src={dialogsData[1].src} message={dialogsData[1].message}/>
+                <Dialogs src={dialogsData[2].src} message={dialogsData[2].message}/>
+                <Dialogs src={dialogsData[3].src} message={dialogsData[3].message}/>
             </div>
         </div>
     )
--- src/Components/profile/Profile.tsx
+++ src/Components/profile/Profile.tsx
@@ -4,20 +4,52 @@ import {Newpost} from './newpost/Newpost';
 import {Post} from './post/Post';
 
 export const Profile = () => {
+    const avatarData = [
+        {
+            id: '1',
+            src: 'https://klike.net/uploads/posts/2019-03/medium/1551511784_4.jpg',
+            name: 'Anita',
+            age: '34',
+            zodiac: 'Libra',
+            professional: 'Front-end Developer',
+        },
+    ]
+    const postData = [
+        {
+            src: 'https://vibir.ru/wp-content/uploads/2019/10/avatarka-dlya-zhenshhin-glavnye-pravila-vybora.jpg',
+            description: 'Hi!', countLike: 15, countDislike: 1,
+        },
+        {
+            src: 'https://vibir.ru/wp-content/uploads/2019/10/avatarka-dlya-zhenshhin-glavnye-pravila-vybora.jpg',
+            description: 'How are you?', countLike: 10, countDislike: 0,
+        },
+        {
+            src: 'https://vibir.ru/wp-content/uploads/2019/10/avatarka-dlya-zhenshhin-glavnye-pravila-vybora.jpg',
+            description: 'I\'m OK, and you?', countLike: 5, countDislike: 0,
+        },
+    ]
     return (
         <div className={style.profile}>
-            <img className={style.img} src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg" alt=""/>
-            <Avatar src = "https://klike.net/uploads/posts/2019-03/medium/1551511784_4.jpg"
-                    name = 'Anita'
-                    age = '34'
-                    zodiac = 'Libra'
-                    professional = 'Front-end Developer'
+            <img className={style.img}
+                 src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"
+                 alt=""/>
+            <Avatar src={avatarData[0].src}
+                    name={avatarData[0].name}
+                    age={avatarData[0].age}
+                    zodiac={avatarData[0].zodiac}
+                    professional={avatarData[0].professional}
             />
             <Newpost/>
             <div className={style.myPosts}>
-                <Post src="https://vibir.ru/wp-content/uploads/2019/10/avatarka-dlya-zhenshhin-glavnye-pravila-vybora.jpg" description={'Hi!'} countLike={15} countDislike={1}/>
-                <Post src="https://vibir.ru/wp-content/uploads/2019/10/avatarka-dlya-zhenshhin-glavnye-pravila-vybora.jpg" description={'How are you?'} countLike={10} countDislike={0}/>
-                <Post src="https://vibir.ru/wp-content/uploads/2019/10/avatarka-dlya-zhenshhin-glavnye-pravila-vybora.jpg" description={"I'm OK, and you?"} countLike={5} countDislike={0}/>
+                <Post
+                    src={postData[0].src}
+                    description={postData[0].description} countLike={postData[0].countLike} countDislike={postData[0].countDislike}/>
+                <Post
+                    src={postData[1].src}
+                    description={postData[1].description} countLike={postData[1].countLike} countDislike={postData[1].countDislike}/>
+                <Post
+                    src={postData[2].src}
+                    description={postData[2].description} countLike={postData[2].countLike} countDislike={postData[2].countDislike}/>
             </div>
         </div>
     )
